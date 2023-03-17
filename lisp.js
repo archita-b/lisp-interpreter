6286c3b51ba0a35ca984db8724bab640b1930992
archita-b <archita.bhattacharyya91@gmail.com>
2023-03-17 10:17:05 +0530

    lisp-interpreter

--- lisp.js
+++ lisp.js
@@ -1,21 +1,69 @@
-const arithmatic = (input) => {
-    input = input.slice(1).trim();
-    const operator = input[0];
-    input = input.slice(1).trim();
-    const num1 = input[0];
-    input = input.slice(1).trim();
-    const num2 = input[0];
-    if (operator === '+') {
-        return Number(num1) + Number(num2);
+const arithmatic = input => {
+    input = input.slice(1);
+    const operator = opParser(input);
+    input = input.slice(operator.length);
+    const args = argParser(input);
+    let result;
+    switch (operator) {
+        case '+':
+            result = args.reduce((a,b) => Number(a) + Number(b), 0);
+            break;
+        case '-':
+            result = args.reduce((a,b) => {
+                // console.log(args.length)          
+                if (args.length === 1) return -args[0];
+                return Number(a) - Number(b)});
+            break;
+        case '*':
+            result = args.reduce((a,b) => Number(a) * Number(b), 1);
+            break;
+        case '/':
+            if (args.length === 2) return args[0] / args[1];
+            return `Expected 2 arguments, got ${args.length}`;
+            break;
+        case '>':
+            if (args.length === 2) return args[0] > args[1];
+            return `Expected 2 arguments, got ${args.length}`;
+            break;
+        case '<':
+            if (args.length === 2) return args[0] < args[1];
+            return `Expected 2 arguments, got ${args.length}`;
+            break;
+        case '>=':
+            if (args.length === 2) return args[0] >= args[1];
+            return `Expected 2 arguments, got ${args.length}`;
+            break;
+        case '<=':
+            if (args.length === 2) return args[0] <= args[1];
+            return `Expected 2 arguments, got ${args.length}`;
+            break;
+        case '=':
+            if (args.length === 2) return args[0] === args[1];
+            return `Expected 2 arguments, got ${args.length}`;
+            break;
     }
-    if (operator === '-') {
-        return Number(num1) - Number(num2);
-    }
-    if (operator === '*') {
-        return Number(num1) * Number(num2);
-    }
-    if (operator === '/') {
-        return Number(num1) / Number(num2);
-    }
+    return result;
 }
-console.log(arithmatic("(/ 1 2)"));
+
+const opParser = input => {
+    let op = '';
+    while (input[0] !== ' ') {
+        op += input[0];
+        input = input.slice(1);
+    }
+    return op;
+}
+// console.log(opParser(">= "));
+
+const argParser = input => {
+    const args = [];
+    while (input[0] !== ')') {
+        // console.log(input[0])
+        args.push(input[0]);
+        input = input.slice(1);
+    }
+    return args.filter(e => e !== ' ');
+}
+// console.log(argParser("1 2 3 4)"));
+
+console.log(arithmatic("(- 5)"));

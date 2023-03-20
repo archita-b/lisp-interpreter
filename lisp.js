ff310c99422ac4bcfe144a79eeea159da92d5c33
archita-b <archita.bhattacharyya91@gmail.com>
2023-03-20 10:21:33 +0530

    lisp-interpreter

--- lisp.js
+++ lisp.js
@@ -1,69 +1,44 @@
+const globalEnv = {
+	'+': args => args.reduce((a, b) => Number(a) + Number(b), 0),
+	'-': args => args.length === 1 ? -args[0] : args.reduce((a, b) => Number(a) - Number(b)),
+	'*': args => args.reduce((a, b) => Number(a) * Number(b), 1),
+	'/': args => args.length === 2 ? args[0] / args[1] : null,
+	'>': args => args.length === 2 ? args[0] > args[1] : null,
+	'<': args => args.length === 2 ? args[0] < args[1] : null,
+	'>=': args => args.length === 2 ? args[0] >= args[1] : null,
+	'<=': args => args.length === 2 ? args[0] <= args[1] : null,
+	'=': args => args.length === 2 ? args[0] = args[1] : null,
+	'sqrt': args => Math.sqrt(args[0])
+}
+
 const arithmatic = input => {
-    input = input.slice(1);
-    const operator = opParser(input);
-    input = input.slice(operator.length);
-    const args = argParser(input);
-    let result;
-    switch (operator) {
-        case '+':
-            result = args.reduce((a,b) => Number(a) + Number(b), 0);
-            break;
-        case '-':
-            result = args.reduce((a,b) => {
-                // console.log(args.length)          
-                if (args.length === 1) return -args[0];
-                return Number(a) - Number(b)});
-            break;
-        case '*':
-            result = args.reduce((a,b) => Number(a) * Number(b), 1);
-            break;
-        case '/':
-            if (args.length === 2) return args[0] / args[1];
-            return `Expected 2 arguments, got ${args.length}`;
-            break;
-        case '>':
-            if (args.length === 2) return args[0] > args[1];
-            return `Expected 2 arguments, got ${args.length}`;
-            break;
-        case '<':
-            if (args.length === 2) return args[0] < args[1];
-            return `Expected 2 arguments, got ${args.length}`;
-            break;
-        case '>=':
-            if (args.length === 2) return args[0] >= args[1];
-            return `Expected 2 arguments, got ${args.length}`;
-            break;
-        case '<=':
-            if (args.length === 2) return args[0] <= args[1];
-            return `Expected 2 arguments, got ${args.length}`;
-            break;
-        case '=':
-            if (args.length === 2) return args[0] === args[1];
-            return `Expected 2 arguments, got ${args.length}`;
-            break;
-    }
-    return result;
+	input = input.slice(1);
+	const operator = opParser(input);
+	input = input.slice(operator.length);
+	const args = argParser(input);
+	let result;
+	if (Object.keys(globalEnv).includes(operator)) {
+		result = globalEnv[operator](args);
+	}
+	return result !== null ? result : `Expected 2 arguments, got ${args.length}`;
 }
 
 const opParser = input => {
-    let op = '';
-    while (input[0] !== ' ') {
-        op += input[0];
-        input = input.slice(1);
-    }
-    return op;
+	let op = '';
+	while (input[0] !== ' ') {
+		op += input[0];
+		input = input.slice(1);
+	}
+	return op;
 }
-// console.log(opParser(">= "));
 
 const argParser = input => {
-    const args = [];
-    while (input[0] !== ')') {
-        // console.log(input[0])
-        args.push(input[0]);
-        input = input.slice(1);
-    }
-    return args.filter(e => e !== ' ');
+	const args = [];
+	while (input[0] !== ')') {
+		args.push(input[0]);
+		input = input.slice(1);
+	}
+	return args.filter(e => e !== ' ');
 }
-// console.log(argParser("1 2 3 4)"));
 
-console.log(arithmatic("(- 5)"));
+console.log(arithmatic("(/ 4 2 1)"));
